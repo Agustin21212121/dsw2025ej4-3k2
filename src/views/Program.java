dff6f006787130dd541e1feb4dd537fcafae81ff
Fernandez Agustin <agustinfernandez02@outlook.es>
2025-04-13 11:14:25 -0300

    Program modificado

--- src/views/Program.java
+++ src/views/Program.java
@@ -10,7 +10,7 @@ public class Program {
 
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarAnimalesView view = new ListarAnimalesView();
-        view.setVisible(true);
+        MenuPrincipalView menu = new MenuPrincipalView();
+        menu.setVisible(true);
     }
 }
