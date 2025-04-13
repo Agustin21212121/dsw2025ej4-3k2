8a0ea21bc64345c4c46c815ebb386275a4f2a671
Fernandez Agustin <agustinfernandez02@outlook.es>
2025-04-13 11:41:54 -0300

    Correccion de errores

--- src/domain/Pais.java
+++ src/domain/Pais.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package domain;
 
-/**
- *
- * @author agust
- */
 public class Pais {
     private final String nombre;
     private final String codigoIso;
